ba8076d0bc95f1353fde6b1b4ea8e9f1a6ec041b
Luis Roberto <oluisjuniorr@gmail.com>
2024-09-04 14:30:28 -0300

    Editado no VSCode
    Tipos de funções

---
+++ funções.rb
@@ -0,0 +1,22 @@
+Funções em Ruby:
+cada linha de codigo que for preciso rodar no terminal do VSCode é só tirar o comentatio
+´´´ 
+nome = "Lus"
+n = 4
+
+# if nome.eql? ("Luis")
+#   puts "Casado com Graziele"
+# else
+#   puts "Ela não vai embora"
+# end
+
+#condição? verdadeiro : falso 
+
+# puts nome.eql? ("Luis") ? "Casado com Graziele" : "Ela não vai embora"
+# resultado =  nome.eql? ("Luis") ? "Casado com Graziele resultado": "Ela não vai embora resultado"
+# puts resultado
+
+soma = n.eql?(5)? + 10 : n-1
+puts soma
+
+```
